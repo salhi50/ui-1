1c53c1c45e855135666f5461330049b32000db1c
salhi20 <96256908+salhi50@users.noreply.github.com>
2022-07-19 22:48:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// header
+
+const rightMenuIcon = document.getElementById("right-menu-icon");
+const navHeader = document.getElementById("nav-header");
+rightMenuIcon.addEventListener("click", () => navHeader.classList.toggle("active"));
